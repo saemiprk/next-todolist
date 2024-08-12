6c5d0701a564b6a1d827856802df19f49a8efa83
Saemi Park <saemiprk@gmail.com>
2024-08-12 16:41:53 +0900

    CRUD

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "config/material-tailwind-theme-provider";
+import ReactQueryClientProvider from "config/ReactClientProivider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,19 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
-        />
-      </head>
-      <ThemeProvider>
-        <body className={inter.className}>{children}</body>
-      </ThemeProvider>
-    </html>
+    <ReactQueryClientProvider>
+      <html lang="en">
+        <head>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+            integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer"
+          />
+        </head>
+        <ThemeProvider>
+          <body className={inter.className}>{children}</body>
+        </ThemeProvider>
+      </html>
+    </ReactQueryClientProvider>
   );
 }
--- components/todo.tsx
+++ components/todo.tsx
@@ -1,16 +1,45 @@
 'use client';
 
-import { Checkbox, IconButton } from "@material-tailwind/react";
+import { Checkbox, IconButton, Spinner } from "@material-tailwind/react";
+import { useMutation } from "@tanstack/react-query";
+import { deleteTodo, updateTodo } from "actions/todo-actions";
+import { queryClient } from "config/ReactClientProivider";
 import { useState } from "react";
 
-export default function Todo(){
+export default function Todo({ todo }){
     const [isEditing, setIsEditing] = useState(false);
-    const [completed, setCompleted] = useState(false);
-    const [title, setTitle] = useState("");
+    const [completed, setCompleted] = useState(todo.completed);
+    const [title, setTitle] = useState(todo.title);
+
+    const updateTodoMutation = useMutation({
+        mutationFn: () => updateTodo({
+            id: todo.id,
+            title,
+            completed,
+        }),
+        onSuccess: () => {
+            setIsEditing(false);
+            queryClient.invalidateQueries({
+                queryKey: ["todos"],
+            });
+        }
+    });
+
+    const deleteTodoMutation = useMutation({
+        mutationFn: () => deleteTodo(todo.id),
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["todos"],
+            });
+        }
+    });
 
     return (
         <div className="w-full flex items-center gap-2">
-            <Checkbox checked={completed} />
+            <Checkbox checked={completed} onChange={async (e) => {
+                await setCompleted(e.target.checked);
+                await updateTodoMutation.mutate();
+                }} />
             {isEditing ? (
                 <input className="flex-1 border-b-black border-b pb-1" type="text" value={title} onChange={(e) => setTitle(e.target.value) } />
             ): (
@@ -18,16 +47,24 @@ export default function Todo(){
             )}
             
             {isEditing ? (
-                <IconButton onClick={() => setIsEditing(false)}>
-                    <i className="fas fa-check" />
+                <IconButton onClick={async () => {
+                    await updateTodoMutation.mutate();
+                }}>
+                    {updateTodoMutation.isPending ? (<Spinner />) : (
+                        <i className="fas fa-check" />
+                    )}
                 </IconButton>
             ) : (
                 <IconButton onClick={() => setIsEditing(true)}>
                     <i className="fas fa-pen" />
                 </IconButton>
             )}
-            <IconButton onClick={() => {}}>
-                <i className="fas fa-trash" />
+            <IconButton onClick={async () => {
+                await deleteTodoMutation.mutate();
+            }}>
+                {updateTodoMutation.isPending ? (<Spinner />): (
+                    <i className="fas fa-trash" />
+                )}
             </IconButton>
         </div>
     )
