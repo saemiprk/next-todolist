bf299d1066173e9f7441d1b448ed5b974e2093ca
Saemi Park <saemiprk@gmail.com>
2024-08-12 13:56:57 +0900

    ui

---
+++ app/ui.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { Button, Input } from "@material-tailwind/react";
+import Todo from "components/todo";
+
+export default function UI(){
+    return (
+        <div className="w-2/3 mx-auto flex flex-col items-center py-10 gap-2">
+            <h1 className="text-xl">TODO LIST</h1>
+            <Input label="Search TODO" icon={<i className="fas fa-search" />} />
+            <Todo id={1} value={'New TODO'} completed={true} />
+
+            <Button>
+                <i className="fas fa-plus mr-2" />
+                Add TODO
+            </Button>
+        </div>
+    )
+}
